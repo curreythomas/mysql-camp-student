97d4eb09f3e289ac8ca5e51cf71b2fc2e6635569
curreythomas <currey.thomas@gmail.com>
2017-10-19 16:00:29 -0400

    exercise 4

--- student-exercises/currey-select.sql
+++ student-exercises/currey-select.sql
@@ -1,4 +1,9 @@
-SELECT * FROM individual WHERE LastName = 'Jennings';
-SELECT ID, LASTNAME FROM individual WHERE DeceasedDate IS NOT NULL;
-SELECT ID, LastName, BirthDate FROM individual WHERE Year(BirthDate) > 1940;
-SELECT * FROM individual WHERE ID IN (1,3,5,7,19);
+-- Create a query that provides a list of alternative bands that have broken up.
+-- 
+-- SELECT * 
+-- FROM band
+-- WHERE genre = 'Alternative' AND isTogether = 0; 
+
+-- Create a query that provides the id and name of the band with an ID of 4.
+
+SELECT ID, name FROM band WHERE ID = 4
