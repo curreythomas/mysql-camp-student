5edc27cfe9e958614b9906f8e163dfbc4ea1bf36
curreythomas <currey.thomas@gmail.com>
2017-10-20 15:35:29 -0400

    order by exercise 3

---
+++ student-exercises/exercise3-orderby.sql
@@ -0,0 +1,12 @@
+-- SELECT player.* FROM player ORDER BY player.firstName DESC;
+
+-- select player.* FROM player ORDER BY player.birthdate ASC;
+
+-- SELECT league, divisionName, teamName, playerName, heightInches FROM vteamroster ORDER BY league, divisionName, teamName, heightInches DESC;
+
+-- SELECT B.battingAvg, B.ID, T.teamName FROM team T INNER JOIN batting B ON T.ID = B.ID 
+
+-- SELECT firstName, lastName, ABBR, hits FROM vbattingleaders ORDER BY hits DESC;
+
+-- SELECT firstName, lastName, ABBR, hits FROM vbattingleaders ORDER BY hits ASC;
+
